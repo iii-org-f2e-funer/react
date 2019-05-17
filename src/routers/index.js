b4bcdf1615980fffed85566f73fab8870d5ecf2d
sujigary <yhybs@hotmail.com>
2019-05-17 14:15:01 +0800

    05170214

--- src/routers/index.js
+++ src/routers/index.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom'
 import React from 'react'
 import Demopage from '../pages/demopage.js'
+import Pt_list from '../pages/Pt_list'
 
 function RootRouter() {
   return (
@@ -19,11 +20,15 @@ function RootRouter() {
           <li>
             <Link to="/demo">demo</Link>
           </li>
+          <li>
+            <Link to="/pt_list">揪團</Link>
+          </li>
         </ul>
 
         <Switch>
           <Route exact path="/" component={Demopage} />
-          <Route path="/about" component={Demopage} />
+          <Route path="/demo" component={Demopage} />
+          <Route path="/pt_list" component={Pt_list} />
         </Switch>
       </>
     </Router>
