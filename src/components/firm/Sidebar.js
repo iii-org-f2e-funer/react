3c9bc8bcfdea3528021c0a17cb13ad1fbf7eb199
op30132 <op30132@gmail.com>
2019-05-17 16:29:26 +0800

    favucon

--- src/components/firm/Sidebar.js
+++ src/components/firm/Sidebar.js
@@ -7,6 +7,7 @@ const Sidebar = () => {
                 <p>桌遊糖果城</p>
                 <p>candycity@gmail.tw</p>
                 <button>編輯店家資料</button>
+                
             </div>
         </>
       )
