fd6108a6c001adc3598834212017ca7677f1aa85
PH-LEE <leepai311@gmail.com>
2019-05-25 23:02:12 +0800

    chatroom update v8

--- src/components/chatroom/pages/ChatArea_socket.js
+++ src/components/chatroom/pages/ChatArea_socket.js
@@ -4,6 +4,7 @@ import { withRouter } from 'react-router'
 import avatar from '../avatar/ironman.jpg'
 import Moment from 'react-moment'
 import socketIOClient from 'socket.io-client'
+import moment from 'moment'
 
 // const PathNow = props => <div>目前位置 {props.location.pathname}</div>;
 
@@ -20,6 +21,8 @@ class ChatArea extends React.Component {
       member_chat_data: [],
       start_chat_time: '',
       endpoint: 'http://localhost:8080',
+      newChatData: [],
+      inputContent: '',
     }
   }
   async componentDidMount() {
@@ -83,15 +86,14 @@ class ChatArea extends React.Component {
       console.log(this.state.chatDataAll)
 
       console.log(theUrl)
-
-      console.log('memberChatData', this.state.member_chat_data)
+      console.log(this.state.member_chat_data)
 
       //socket sever interact
       socket.emit(
         'join',
-        this.state.member_chat_data.sender_id +
+        this.state.member_chat_data[0].sender_id +
           '_' +
-          this.state.member_chat_data.receiver_id
+          this.state.member_chat_data[0].receiver_id
       )
       socket.on('join', data => {
         console.log(data)
@@ -99,6 +101,33 @@ class ChatArea extends React.Component {
     } catch (e) {
       console.log(e)
     }
+  }
+  handleChange = event => {
+    console.log(event.target.value)
+    this.setState({ inputContent: event.target.value })
+  }
+  handleClick = async () => {
+    var tzoffset = new Date().getTimezoneOffset() * 60000 //offset in milliseconds
+    var localISOTime = new Date(Date.now() - tzoffset)
+      .toISOString()
+      .replace(/\.\d{3}\Z/, ' ')
+      .replace('T', ' ')
+
+    console.log(localISOTime)
+    console.log(this.state.member_chat_data[0].m_time)
+    let chatData = {
+      m_cont: this.state.inputContent,
+      m_time: localISOTime,
+      m_issender: 1,
+    }
+    let copyNewChatData = [chatData, ...this.state.newChatData]
+    let copyMemberChatData = [chatData, ...this.state.member_chat_data]
+    await this.setState({
+      newChatData: copyNewChatData,
+      inputContent: '',
+      member_chat_data: copyMemberChatData,
+    })
+    console.log(this.state.newChatData)
   }
 
   render() {
@@ -125,11 +154,7 @@ class ChatArea extends React.Component {
                     <div className="text-box sender align-items-center">
                       <h5 className="my-auto rounded-pill">{element.m_cont}</h5>
                     </div>
-                    <small>
-                      <Moment format="YYYY-MM-DD HH:MM:SS">
-                        {element.m_time}
-                      </Moment>
-                    </small>
+                    <small>{element.m_time}</small>
                   </li>
                 ) : (
                   <li key={+element.m_time + 1}>
@@ -139,11 +164,7 @@ class ChatArea extends React.Component {
                       </div>
                       <h5 className="my-auto rounded-pill">{element.m_cont}</h5>
                     </div>
-                    <small>
-                      <Moment format="YYYY-MM-DD HH:MM:SS">
-                        {element.m_time}
-                      </Moment>
-                    </small>
+                    <small>{element.m_time}</small>
                   </li>
                 )
               })}
@@ -170,8 +191,19 @@ class ChatArea extends React.Component {
             </ul>
           </div>
           <div className="send_box d-flex ">
-            <input className="blue" type="text" />
-            <button className="button button--yellow">傳送</button>
+            <input
+              className="blue"
+              type="text"
+              value={this.state.inputContent}
+              onChange={this.handleChange}
+              autoFocus
+            />
+            <button
+              className="button button--yellow"
+              onClick={this.handleClick}
+            >
+              傳送
+            </button>
           </div>
         </div>
       </>
