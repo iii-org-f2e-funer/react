af672bce7907e04d4633bd245627ecc0958c54a6
jerry905925 <jerry905925@gmail.co>
2019-05-28 02:42:15 +0800

    add fav bookmark

--- src/components/instagram/OldComment.js
+++ src/components/instagram/OldComment.js
@@ -29,7 +29,6 @@ class OldComment extends React.Component {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
-          'user-agent': 'Mozilla/4.0 MDN Example',
           'content-type': 'application/json',
         },
       })
@@ -43,7 +42,7 @@ class OldComment extends React.Component {
         })
     }
   }
- 
+
   render() {
     return (
       <>
--- src/components/instagram/Stories.js
+++ src/components/instagram/Stories.js
@@ -7,6 +7,7 @@ class Stories extends React.Component {
     super()
     this.state = {
       stories: [],
+      storyState: [],
     }
   }
 
@@ -20,32 +21,81 @@ class Stories extends React.Component {
     clearInterval(this.timer1)
   }
   fetchAllData = () => {
-    // fetch('http://localhost:3002/instagram/getStories')
+    var userID = { userId: '1' }
+    var allStories = []
+    var allState = []
+    // fetch 所有貼文
     fetch('http://localhost:3002/instagram/allData')
       .then(res => res.json())
       .then(data => {
         // console.log(data)
-        this.setState({ stories: data })
+        allStories = data
+        // fetch 貼文狀態(favorite bookmark editable)
+        return fetch('http://localhost:3002/instagram/storyState', {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify(userID),
+        })
+      })
+      .then(res => res.json())
+      .then(data => {
+        allState = data
+        this.setState({ stories: allStories, storyState: allState })
       })
   }
+
   handleReFresh = () => {
     this.fetchAllData()
   }
   render() {
-    return (
-      <>
-        <div className="stories">
-          <NewStory handleReFresh={this.handleReFresh} />
-          {this.state.stories.map(item => (
-            <OldStory
-              key={item.post_id}
-              data={item}
-              handleReFresh={this.fetchAllData}
-            />
-          ))}
-        </div>
-      </>
-    )
+    if (this.state.storyState.length !== 0) {
+      return (
+        <>
+          <div className="stories">
+            <NewStory handleReFresh={this.handleReFresh} />
+            {this.state.stories.map(item => (
+              <OldStory
+                key={item.post_id}
+                data={item}
+                handleReFresh={this.fetchAllData}
+                isFav={
+                  this.state.storyState[0].indexOf(item.post_id) > -1
+                    ? true
+                    : false
+                }
+                isBook={
+                  this.state.storyState[1].indexOf(item.post_id) > -1
+                    ? true
+                    : false
+                }
+                editable={
+                  this.state.storyState[2].indexOf(item.post_id) > -1
+                    ? true
+                    : false
+                }
+              />
+            ))}
+          </div>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <div className="stories">
+            <NewStory handleReFresh={this.handleReFresh} />
+            {this.state.stories.map(item => (
+              <OldStory
+                key={item.post_id}
+                data={item}
+                handleReFresh={this.fetchAllData}
+              />
+            ))}
+          </div>
+        </>
+      )
+    }
   }
 }
 export default Stories
