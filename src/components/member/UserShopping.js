e884d77d63bf170d9dbc194aa5c38a0f4332e798
ian8924 <justforian0915@gmail.com>
2019-05-30 20:00:01 +0800

    v3

--- src/components/member/UserShopping.js
+++ src/components/member/UserShopping.js
@@ -1,8 +1,46 @@
 // 揪團部分
 import React from 'react'
 import { withRouter } from 'react-router'
+import actions from '../../redux/action/userInfo.js'
+import { connect } from 'react-redux'
+import Account from '../../components/firm/Account'
 
 class UserShopping extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [],
+      data1: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('//localhost:3002/member/productorder', {})
+      //fetch prodct_manage
+      .then(response => {
+        // 這裡會得到一個 ReadableStream 的物件
+        // console.log(response)
+        // 可以透過 blob(), json(), text() 轉成可用的資訊
+        return response.json()
+      })
+      .then(jsonData => {
+        this.setState({ data: jsonData })
+        var d2_leng = Object.keys(jsonData).length
+        var aa = []
+        // for (let i = d2_leng - 1; i >= 0; i--) {
+        //   if (this.props.userInfo.account == jsonData[i]) {
+        //     aa.push(jsonData[i])
+        //   }
+        // }
+        console.log(jsonData)
+        console.log(aa)
+        // typeof()
+        // console.log(this.state.data)
+      })
+      .catch(err => {
+        // console.log('錯誤:', err)
+      })
+  }
   render() {
     return (
       <>
@@ -11,5 +49,16 @@ class UserShopping extends React.Component {
     )
   }
 }
-
-export default withRouter(UserShopping)
+function mapStateToProp(store) {
+  return {
+    userInfo: store.userInfo,
+  }
+}
+export default withRouter(
+  connect(
+    mapStateToProp,
+    {
+      userInfoAction: actions.userInfo,
+    }
+  )(UserShopping)
+)
